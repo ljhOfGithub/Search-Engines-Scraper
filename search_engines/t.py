841adf306789199531f2f6a2bcfa67544c4ccc11
ljhOfGithub <42089575+ljhOfGithub@users.noreply.github.com>
2022-04-10 22:04:00 +0800

    add winsearch

--- search_engines/t.py
+++ search_engines/t.py
@@ -1,11 +1,21 @@
 from search_engines import Google
 from ast import literal_eval
 import time
+import argparse
+import os
 # import arg
 # def mysearch(csvaddr='0x5b27531228d8c65a0d2adcd8903fd3348f768a11'):
 # def mysearch(csvaddr='0xb01fce059d66971fdc1e584a5cbf0116068c9048'):
 # def mysearch(csvaddr='0x81e4b65b9330c5693d38430111d7eb174615bdd6'):
-def mysearch(csvaddr='0x98831a269cb88b6bfcc86e45b32c158981b34b22'):
+# def mysearch(csvaddr='0x98831a269cb88b6bfcc86e45b32c158981b34b22'):
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-q', help='query to search', default='0x3bf351a62df57dce8512b136daa4cd6ebe2dda91')
+    args = ap.parse_args()
+    csvaddr = args.q
+    mysearch(csvaddr)
+
+def mysearch(csvaddr='0x3bf351a62df57dce8512b136daa4cd6ebe2dda91'):
     with open('addr.txt','r') as f:
         addrlist = literal_eval(f.read())
     # addrlist = ['0x516980e3321482b51b0e10af2770d6fbd47f6f9f']
@@ -26,19 +36,51 @@ def mysearch(csvaddr='0x98831a269cb88b6bfcc86e45b32c158981b34b22'):
             filename = 'mycsv/' + addr
             engine.output('csv',filename)
             links = results.links()
-            time.sleep(0.5)
+            time.sleep(2)
         except Exception:
             print('reboot')
             time.sleep(5)
             myindex = addrlist.index(addr) - 1#
             rebootaddr = addrlist[myindex]
             count += 1
-            if count >= 50 :
+            if count >= 50:
                 quit()
-            mysearch(rebootaddr)
+            # mysearch(rebootaddr)
+            os.execv('/root/.miniconda3/envs/twvenv/bin/python',['python','t.py','-q',rebootaddr])
+
             # print(addr)
             # results = engine.search(addr)
             # engine.output('html',filename)
     # print(links)
+def winsearch():
+    with open('addr.txt','r') as f:
+        addrlist = literal_eval(f.read())
+        csvaddr = '0x3bf351a62df57dce8512b136daa4cd6ebe2dda91'
+        count = 0
+        myindex = addrlist.index(csvaddr)
+        addrlist = addrlist[myindex:]
+        engine = Google()
+    for addr in addrlist:
+        try:
+            engine.set_search_operator('host')  # 设置为将对应的host过滤掉
+            results = engine.search(addr)
+            filename = 'mycsv/' + addr
+            engine.output('csv', filename)
+            links = results.links()
+            time.sleep(2)
+        except Exception:
+            print('reboot')
+            time.sleep(5)
+            myindex = addrlist.index(addr) - 1  #
+            rebootaddr = addrlist[myindex]
+            count += 1
+            if count >= 50:
+                quit()
+            mysearch(rebootaddr)
+            # os.execv('/root/.miniconda3/envs/twvenv/bin/python', ['python', 't.py', '-q', rebootaddr])
 if __name__ == '__main__':
-    mysearch()
+    # mysearch()
+    # main()
+    winsearch()
+
+#python t.py -q 0x3bf351a62df57dce8512b136daa4cd6ebe2dda91
